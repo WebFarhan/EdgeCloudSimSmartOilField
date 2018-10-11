9010897cb69cdf4e55f4927b09df16051f90cf35
Hussain Razin Farhan <c00303945@moshenlab1.win.louisiana.edu>
2018-10-11 10:53:39 -0500

    first commit

---
+++ src/edu/boun/edgecloudsim/edge_orchestrator/BasicEdgeOrchestrator.java
@@ -0,0 +1,386 @@
+/*
+ * Title:        EdgeCloudSim - Basic Edge Orchestrator implementation
+ * 
+ * Description: 
+ * BasicEdgeOrchestrator implements basic algorithms which are
+ * first/next/best/worst/random fit algorithms while assigning
+ * requests to the edge devices.
+ *               
+ * Licence:      GPL - http://www.gnu.org/copyleft/gpl.html
+ * Copyright (c) 2017, Bogazici University, Istanbul, Turkey
+ */
+
+package edu.boun.edgecloudsim.edge_orchestrator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+import org.apache.commons.math3.distribution.NormalDistribution;
+import org.apache.commons.math3.exception.NotStrictlyPositiveException;
+import org.cloudbus.cloudsim.Datacenter;
+import org.cloudbus.cloudsim.core.CloudSim;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
+
+import edu.boun.edgecloudsim.core.SimManager;
+import edu.boun.edgecloudsim.core.SimSettings;
+import edu.boun.edgecloudsim.edge_server.EdgeHost;
+import edu.boun.edgecloudsim.edge_server.EdgeVM;
+import edu.boun.edgecloudsim.edge_client.CpuUtilizationModel_Custom;
+import edu.boun.edgecloudsim.edge_client.Task;
+import edu.boun.edgecloudsim.utils.ETCMatrix;
+import edu.boun.edgecloudsim.utils.ETTMatrix;
+import edu.boun.edgecloudsim.utils.Location;
+import edu.boun.edgecloudsim.utils.SimLogger;
+
+public class BasicEdgeOrchestrator extends EdgeOrchestrator {
+	private int numberOfHost; //used by load balancer
+	@SuppressWarnings("unused")
+	private int lastSelectedHostIndex; //used by load balancer
+	private int[] lastSelectedVmIndexes; //used by each host individually
+	private static Datacenter receivingBS; // !!! IMPORTANT !!! DON'T USE THE METHOD Datacenter.getId(), it's messed up, use recBS instead if u need an index.
+	private int recBS = -1; //Receiving DC ID
+	private ArrayList<Integer> neighboringBS = new ArrayList<>(); //Neighboring BaseStations
+	
+	private static ETCMatrix matrix;
+	private static ETTMatrix ettmatrix;
+	
+	public BasicEdgeOrchestrator(String _policy, String _simScenario) {
+		super(_policy, _simScenario);
+	}
+
+	@Override
+	public void initialize() {
+		numberOfHost=SimSettings.getInstance().getNumOfEdgeHosts();
+		
+		lastSelectedHostIndex = -1;
+		lastSelectedVmIndexes = new int[numberOfHost];
+		for(int i=0; i<numberOfHost; i++)
+			lastSelectedVmIndexes[i] = -1;
+	}
+	
+	private static double getConvolveProbability(double mu, double sigma, double deadLine) {
+		
+		try {
+		NormalDistribution distr = new NormalDistribution(mu, sigma);
+		return distr.cumulativeProbability(deadLine);
+		}catch(NotStrictlyPositiveException exc) {
+			return 0.0;
+		}
+	
+	}
+	
+
+	@Override
+	public int getDeviceToOffload(Task task) {
+		
+		int result = SimSettings.GENERIC_EDGE_DEVICE_ID;
+		matrix = SimLogger.getInstance().matrix;
+		double dl = 0.0;
+		
+		//int result = SimSettings.EDGE_ORCHESTRATOR_ID;
+		if(simScenario.equals("SINGLE_TIER")){
+			result = SimSettings.GENERIC_EDGE_DEVICE_ID;
+		}
+		else {
+			result = SimSettings.EDGE_ORCHESTRATOR_ID;
+		}
+	
+		/* Calculate the location of receiving Base Station
+		 * the closest one to the device(vehicle)
+		 */
+		
+		Location deviceLoc = SimManager.getInstance().getMobilityModel().getLocation(task.getMobileDeviceId(), CloudSim.clock());
+		int xdev = deviceLoc.getXPos();
+		int ydev = deviceLoc.getYPos();
+		
+		List<Datacenter> datacenters = SimManager.getInstance().edgeServerManager.getDatacenterList();
+		
+		double best = 1000;
+
+		for(int i = 0; i < datacenters.size(); i++) {
+			List<EdgeHost> hostlist = datacenters.get(i).getHostList();
+			
+			for(EdgeHost host : hostlist) {
+				Location hostLocation = host.getLocation();
+				int xhost = hostLocation.getXPos();
+				int yhost = hostLocation.getYPos();
+				double dist = Math.sqrt((Math.pow((double)xdev-xhost, 2))+ (Math.pow((double)ydev-yhost, 2)));
+				if (dist <= best) {
+					best = dist;
+					setReceivingBS(datacenters.get(i));
+					recBS = i;
+					task.setDc(i);
+					dl = SimManager.getInstance().getEdgeOrchestrator().deadline(task, matrix, 0.0001,recBS);
+					//task.setDeadLine(dl);
+					}
+				}
+			}
+		
+		return result;
+	}
+	
+	@Override
+	public EdgeVM getVmToOffload(Task task) {
+		if(simScenario.equals("TWO_TIER_WITH_EO"))
+			return selectVmOnLoadBalancer(task);
+		else
+			return selectVmOnHost(task);
+	}
+	
+	/*
+	 * The base case policy;
+	 */
+	
+	public EdgeVM selectVmOnHost(Task task){
+		
+		EdgeVM selectedVM = null;
+		List<EdgeVM> vmArray = receivingBS.getVmList();
+		
+		for(int i = 0; i < vmArray.size(); i++) {
+			double requiredCapacity = ((CpuUtilizationModel_Custom)task.getUtilizationModelCpu()).predictUtilization(vmArray.get(i).getVmType());
+			double targetVmCapacity = (double)100 - vmArray.get(i).getCloudletScheduler().getTotalUtilizationOfCpu(CloudSim.clock());
+			if(requiredCapacity <= targetVmCapacity)
+				selectedVM = vmArray.get(i);
+			}
+
+		
+		return selectedVM;
+	}
+	
+	/*
+	 * Load Balancer Policy;
+	 */
+
+	public EdgeVM selectVmOnLoadBalancer(Task task){
+		EdgeVM selectedVM = null;
+		//getNeighbors(task);
+		//System.out.print(receivingBS.getId()+ "\n");
+		//EdgeVM selectedVM  = getDC(task);
+		
+		if(policy.equalsIgnoreCase("Probability")) {
+			getNeighbors(task);
+			selectedVM  = getDC(task);
+			
+		}
+		else if(policy.equalsIgnoreCase("Baseline")){
+			Location deviceLocation = SimManager.getInstance().getMobilityModel().getLocation(task.getMobileDeviceId(), CloudSim.clock());
+			int relatedHostId=deviceLocation.getServingWlanId();
+			List<EdgeVM> vmArray = SimManager.getInstance().getLocalServerManager().getVmList(relatedHostId);
+			for(int i = 0; i < vmArray.size(); i++) {
+				double requiredCapacity = ((CpuUtilizationModel_Custom)task.getUtilizationModelCpu()).predictUtilization(vmArray.get(i).getVmType());
+				double targetVmCapacity = (double)100 - vmArray.get(i).getCloudletScheduler().getTotalUtilizationOfCpu(CloudSim.clock());
+				if(requiredCapacity <= targetVmCapacity)
+					selectedVM = vmArray.get(i);
+			}
+			
+		}
+		else if(policy.equalsIgnoreCase("MECT")) {
+			
+			Datacenter dc = SimManager.getInstance().edgeServerManager.getDatacenterList().get(getMectDC(task));
+			List<EdgeVM> vmArrayMect = dc.getVmList();
+			for(int i = 0; i < vmArrayMect.size(); i++) {
+				double requiredCapacity = ((CpuUtilizationModel_Custom)task.getUtilizationModelCpu()).predictUtilization(vmArrayMect.get(i).getVmType());
+				double targetVmCapacity = (double)100 - vmArrayMect.get(i).getCloudletScheduler().getTotalUtilizationOfCpu(CloudSim.clock());
+				if(requiredCapacity <= targetVmCapacity) {
+					selectedVM = vmArrayMect.get(i);
+					}
+				}
+			
+		}
+		
+		return selectedVM;
+		
+	}
+	
+	/*
+	 * Calculate the deadline for the task
+	 * 
+	 */
+	
+	public double deadline(Task task, ETCMatrix b, double slack, int targetBS) {
+		
+		double comDelay = SimManager.getInstance().getNetworkModel().getUploadDelay(task.getMobileDeviceId(), targetBS ,task.getCloudletFileSize()) + 
+							SimManager.getInstance().getNetworkModel().getDownloadDelay(targetBS, task.getMobileDeviceId(), task.getCloudletFileSize());
+		double submissionTime = task.getSubmissionTime();
+		double avgMu = 0;
+		for(int i = 0; i < b.getDataCnum(); i++) {
+			avgMu += b.getMu(i, task.getTaskType().ordinal());
+		}
+		double avgMuAll = avgMu/b.getDataCnum();
+		double beta = 0.8;
+		double alpha = 0.9;
+		double deadline =  (beta*avgMuAll)+ slack + submissionTime + alpha*comDelay;
+		task.setDeadLine(deadline);
+		return deadline;
+	}
+	
+	
+	public double deadlineTransfer(Task task, ETCMatrix b, double slack, int targetBS, int sourceBS) {
+		
+		double comDelay = SimManager.getInstance().getNetworkModel().getUploadDelay(sourceBS, targetBS ,task.getCloudletFileSize()) + 
+							SimManager.getInstance().getNetworkModel().getDownloadDelay(targetBS, sourceBS, task.getCloudletFileSize());
+		double submissionTime = task.getSubmissionTime();
+		double avgMu = 0;
+		for(int i = 0; i < b.getDataCnum(); i++) {
+			avgMu += b.getMu(i, task.getTaskType().ordinal());
+		}
+		double avgMuAll = avgMu/b.getDataCnum();
+		double beta = 0.8;
+		double alpha = 0.9;
+		double deadline =  beta*avgMuAll+ slack + submissionTime + alpha*comDelay;
+		task.setDeadLine(deadline);
+		return deadline;
+	}
+	
+	
+	
+	public EdgeVM getDC(Task task) {
+		
+		EdgeVM selectedVM = null;
+		matrix = SimLogger.getInstance().matrix;
+		ettmatrix = SimLogger.getInstance().ettMatrix;
+
+		double dl1 = 0.0;
+		double dl = SimManager.getInstance().getEdgeOrchestrator().deadline(task, matrix, 0.0001,recBS);
+		double bestProb = matrix.getProbability(recBS, task.getTaskType().ordinal(), dl);
+		
+		//SimLogger.printLine(" Probability in receving DC "+ bestProb);
+		
+		List<EdgeVM> recvmArray = receivingBS.getVmList();
+		
+		for(int j = 0; j < recvmArray.size(); j++) {
+			double requiredCapacity = ((CpuUtilizationModel_Custom)task.getUtilizationModelCpu()).predictUtilization(recvmArray.get(j).getVmType());
+			double targetVmCapacity = (double)100 - recvmArray.get(j).getCloudletScheduler().getTotalUtilizationOfCpu(CloudSim.clock());
+			if(requiredCapacity <= targetVmCapacity) {
+				selectedVM = recvmArray.get(j);
+				}
+		}
+		
+		double[] probContainer = new double[neighboringBS.size()]; 
+		
+		double convMu = 0.0;
+		double convVariance = 0.0;
+		
+		for(int i = 0; i < neighboringBS.size(); i++) {
+			
+			convMu = matrix.getMu(neighboringBS.get(i),  task.getTaskType().ordinal())+ettmatrix.getMu(neighboringBS.get(i), task.getTaskType().ordinal());
+			
+			convVariance = Math.pow(matrix.getSigma(neighboringBS.get(i), task.getTaskType().ordinal()),2)+Math.pow(ettmatrix.getSigma(neighboringBS.get(i), task.getTaskType().ordinal()),2);
+			
+			dl1 = deadlineTransfer(task, matrix, 0.0001,recBS,neighboringBS.get(i)); // 
+			double prob; //= matrix.getProbability(neighboringBS.get(i), task.getTaskType().ordinal(), dl1);
+			
+			prob = getConvolveProbability(convMu, Math.sqrt(convVariance), dl1);
+			
+			probContainer[i] = prob;
+			
+			if (prob > bestProb || selectedVM == null) {
+				int dc = neighboringBS.get(i);
+				List<EdgeVM> vmArray = SimManager.getInstance().edgeServerManager.getDatacenterList().get(dc).getVmList();
+				
+				for(int j = 0; j < vmArray.size(); j++) {
+					double requiredCapacity = ((CpuUtilizationModel_Custom)task.getUtilizationModelCpu()).predictUtilization(vmArray.get(j).getVmType());
+					double targetVmCapacity = (double)100 - vmArray.get(j).getCloudletScheduler().getTotalUtilizationOfCpu(CloudSim.clock());
+					if(requiredCapacity <= targetVmCapacity) {
+						selectedVM = vmArray.get(i);
+						}
+					}
+			 }
+		
+		}
+		
+		Arrays.sort(probContainer);
+		
+		/*
+		SimLogger.printLine(" Number of neighbour "+ neighboringBS.size());
+
+		for(int j=neighboringBS.size()-1;j>=0;j--) {
+			
+			SimLogger.printLine( " probability in DC "+ neighboringBS.get(j)+" is "+ probContainer[j]);
+		}
+		
+		SimLogger.printLine("");
+		SimLogger.printLine("");
+		*/
+		
+		if(selectedVM == null) {
+			
+			SimLogger.printLine("Selected VM is NULL");
+		}
+		
+		neighboringBS.clear();
+		return selectedVM;
+	}
+	
+	public int getMectDC(Task task) {
+		
+		ETCMatrix matrix = SimLogger.getInstance().matrix;
+		int mectDC = recBS;
+		double compValue = 99999;
+		double tmpValue = 0;
+		
+		List<Datacenter> dcs = SimManager.getInstance().edgeServerManager.getDatacenterList();
+		
+		for(int i = 0; i < dcs.size(); i++) {
+			tmpValue = matrix.getMu(i, task.getTaskType().ordinal());
+			
+			if(compValue > tmpValue) {
+				compValue = tmpValue;
+				mectDC = i;
+				double ddll = SimManager.getInstance().getEdgeOrchestrator().deadline(task, matrix, 0.0,mectDC);
+				double mectComD = SimManager.getInstance().getNetworkModel().getUploadDelay(task.getMobileDeviceId(), mectDC ,task.getCloudletFileSize())+SimManager.getInstance().getNetworkModel().getDownloadDelay(mectDC, task.getMobileDeviceId(), task.getCloudletFileSize());
+				
+				double last = ddll-mectComD;
+				task.setDeadLine(last);
+				
+			}
+		}
+		
+		//SimLogger.printLine("Deadline of task "+task.getDeadLine());
+		
+		return mectDC;
+	}
+	
+	/*
+	 * Fill the ArrayList for neighboring Base Stations;
+	 */
+	
+	public void getNeighbors(Task task) {
+		
+		List<EdgeHost> recLoc = receivingBS.getHostList();
+		Location recLocation = recLoc.get(0).getLocation();
+		int xRec = recLocation.getXPos();
+		int yRec = recLocation.getYPos();
+		
+		//SimLogger.printLine(" receving bs location X :"+ xRec +" y :" +yRec);
+		
+		List<Datacenter> datacenters = SimManager.getInstance().edgeServerManager.getDatacenterList();
+
+		for(int i = 0; i < datacenters.size(); i++) {
+			List<EdgeHost> neighbourlist = datacenters.get(i).getHostList();
+			Location neighLocation = neighbourlist.get(0).getLocation();
+			int xNeigh = neighLocation.getXPos();
+			int yNeigh = neighLocation.getYPos();
+			
+			//SimLogger.printLine(" neighbour bs location X :"+ xNeigh +" y :" +yNeigh);
+			
+			int xdiff = xRec-xNeigh;
+			int ydiff = yRec-yNeigh;
+			if(Math.abs(xdiff) == 1) {
+				neighboringBS.add(i);		
+			}
+		}
+		//SimLogger.printLine(" Neighbours of task "+task.getCloudletId() +" "+neighboringBS);
+	}
+
+	public Datacenter getReceivingBS() {
+		return receivingBS;
+	}
+
+	public static void setReceivingBS(Datacenter datacenter) {
+		receivingBS = datacenter;
+	}
+}
---
+++ src/edu/boun/edgecloudsim/sample_application/mainApp.java
@@ -0,0 +1,147 @@
+/*
+ * Title:        EdgeCloudSim - Sample Application
+ * 
+ * Description:  Sample application for EdgeCloudSim
+ *               
+ * Licence:      GPL - http://www.gnu.org/copyleft/gpl.html
+ * Copyright (c) 2017, Bogazici University, Istanbul, Turkey
+ */
+
+package edu.boun.edgecloudsim.sample_application;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.core.CloudSim;
+
+import edu.boun.edgecloudsim.core.ScenarioFactory;
+import edu.boun.edgecloudsim.core.SimManager;
+import edu.boun.edgecloudsim.core.SimSettings;
+import edu.boun.edgecloudsim.edge_client.MobileDeviceManager;
+import edu.boun.edgecloudsim.utils.SimLogger;
+import edu.boun.edgecloudsim.utils.SimUtils;
+
+public class mainApp {
+	
+	/**
+	 * Creates main() to run this example
+	 */
+	public static void main(String[] args) {
+		//disable console output of cloudsim library
+		Log.disable();
+		
+		//enable console ourput and file output of this application
+		SimLogger.enablePrintLog();
+		
+		int iterationNumber = 1;
+		String configFile = "";
+		String outputFolder = "";
+		String edgeDevicesFile = "";
+		String applicationsFile = "";
+		if (args.length == 5){
+			configFile = args[0];
+			edgeDevicesFile = args[1];
+			applicationsFile = args[2];
+			outputFolder = args[3];
+			iterationNumber = Integer.parseInt(args[4]);
+		}
+		else{
+			SimLogger.printLine("Simulation setting file, output folder and iteration number are not provided! Using default ones...");
+			configFile = "scripts/sample_application/config/default_config.properties";
+			applicationsFile = "scripts/sample_application/config/applications.xml";
+			edgeDevicesFile = "scripts/sample_application/config/edge_devices.xml";
+			outputFolder = "sim_results/ite" + iterationNumber;
+		}
+
+		//load settings from configuration file
+		SimSettings SS = SimSettings.getInstance();
+		if(SS.initialize(configFile, edgeDevicesFile, applicationsFile) == false){
+			SimLogger.printLine("cannot initialize simulation settings!");
+			System.exit(0);
+		}
+		
+		if(SS.getFileLoggingEnabled()){
+			SimLogger.enableFileLog();
+			SimUtils.cleanOutputFolder(outputFolder);
+		}
+		
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date SimulationStartDate = Calendar.getInstance().getTime();
+		String now = df.format(SimulationStartDate);
+		SimLogger.printLine("Simulation started at " + now);
+		SimLogger.printLine("----------------------------------------------------------------------");
+		
+		ArrayList<Double> arrli1;
+
+		for(int j=SS.getMinNumOfMobileDev(); j<=SS.getMaxNumOfMobileDev(); j+=SS.getMobileDevCounterSize())
+		{
+			for(int k=0; k<SS.getSimulationScenarios().length; k++)
+			{
+				for(int i=0; i<SS.getOrchestratorPolicies().length; i++)
+				{
+					arrli1 = new ArrayList<Double>(SS.getOrchestratorPolicies().length);
+					for(int loopIndex =0;loopIndex<1;loopIndex++) {
+					
+					String simScenario = SS.getSimulationScenarios()[k];
+					String orchestratorPolicy = SS.getOrchestratorPolicies()[i];
+					Date ScenarioStartDate = Calendar.getInstance().getTime();
+					now = df.format(ScenarioStartDate);
+					
+					SimLogger.printLine("Scenario started at " + now);
+					SimLogger.printLine("Scenario: " + simScenario + " - Policy: " + orchestratorPolicy + " - #iteration: " + loopIndex);
+					SimLogger.printLine("Duration: " + SS.getSimulationTime()/3600 + " hour(s) - Poisson: " + SS.getTaskLookUpTable()[0][2] + " - #devices: " + j);
+					SimLogger.getInstance().simStarted(outputFolder,"SIMRESULT_" + simScenario + "_"  + orchestratorPolicy + "_" + j + "DEVICES");
+					
+					try
+					{
+						// First step: Initialize the CloudSim package. It should be called
+						// before creating any entities.
+						int num_user = 2;   // number of grid users
+						Calendar calendar = Calendar.getInstance();
+						boolean trace_flag = false;  // mean trace events
+				
+						// Initialize the CloudSim library
+						CloudSim.init(num_user, calendar, trace_flag, 0.01);
+						
+						// Generate EdgeCloudsim Scenario Factory
+						ScenarioFactory sampleFactory = new SampleScenarioFactory(j,SS.getSimulationTime(), orchestratorPolicy, simScenario,j);
+						
+						// Generate EdgeCloudSim Simulation Manager
+						SimManager manager = new SimManager(sampleFactory, j, simScenario);
+						
+						// Start simulation
+						manager.startSimulation();
+					}
+					catch (Exception e)
+					{
+						SimLogger.printLine("The simulation has been terminated due to an unexpected error");
+						e.printStackTrace();
+						System.exit(0);
+					}
+					
+					Date ScenarioEndDate = Calendar.getInstance().getTime();
+					now = df.format(ScenarioEndDate);
+					SimLogger.printLine("Scenario finished at " + now +  ". It took " + SimUtils.getTimeDifference(ScenarioStartDate,ScenarioEndDate));
+					SimLogger.printLine("----------------------------------------------------------------------");
+				
+					double d = SimLogger.getInstance().getFailedTask();
+					arrli1.add(d);
+					
+					SimLogger.printLine( " Number failed tasks "+ SimLogger.getInstance().getDlMisCounter());
+					}
+					SimLogger.printLine(" failed list "+arrli1);
+					arrli1.clear();
+				
+				}//End of orchestrators loop
+			}//End of scenarios loop
+		}//End of mobile devices loop
+
+		Date SimulationEndDate = Calendar.getInstance().getTime();
+		now = df.format(SimulationEndDate);
+		SimLogger.printLine("Simulation finished at " + now +  ". It took " + SimUtils.getTimeDifference(SimulationStartDate,SimulationEndDate));
+	}
+}
